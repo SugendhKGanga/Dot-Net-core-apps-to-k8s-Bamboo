096ce9b953ef8cf63636f36f98ca316be2c6a5d1
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2018-06-09 00:17:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#create env if not present
 /usr/local/bin/kubectl create ns test
+
+/usr/local/bin/kubectl run hello-dotnet --image=sugendh/dotnetcorebmb:latest --port=8080 -n test
+sleep 60
+/usr/local/bin/kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n test
