eb75dd18a85f4a068115f152e3b087dc856851f3
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2018-06-12 19:39:10 +0530

    Update deploy-production.sh

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -7,8 +7,13 @@
 #sleep 60
 #kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n production
 
-rm -rf helm
-helm repo add helm http://35.224.229.155/artifactory/helm-local
-helm repo update
-helm upgrade --install --namespace production dotnetcorebmb-latest-prod --set image.repository=sugendh/dotnetcorebmb,image.tag=latest helm/devops
+#rm -rf helm
+#helm repo add helm http://35.224.229.155/artifactory/helm-local
+#helm repo update
+
+#helm install
+/usr/local/bin/helm install --namespace production --name dotnetcorebmb-latest-prod --set image.repository=sugendh/dotnetcorebmb,image.tag=latest helm/devops
+
+#helm upgrade
+#helm upgrade --install --namespace production dotnetcorebmb-latest-prod --set image.repository=sugendh/dotnetcorebmb,image.tag=latest helm/devops
 
