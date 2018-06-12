5f06c95321669c24c20a8ef2cb2efacd0fee1d16
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2018-06-12 19:11:02 +0530

    Update deploy-stage.sh

--- scripts/deploy-stage.sh
+++ scripts/deploy-stage.sh
@@ -7,7 +7,7 @@
 #sleep 60
 #kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n staging
 
-rm -rf helm
-/usr/local/bin/helm repo add helm http://35.224.229.155/artifactory/helm-local
-/usr/local/bin/helm repo update
+#rm -rf helm
+#/usr/local/bin/helm repo add helm http://35.224.229.155/artifactory/helm-local
+#/usr/local/bin/helm repo update
 /usr/local/bin/helm upgrade --install --namespace staging dotnetcorebmb-latest-stage --set image.repository=sugendh/dotnetcorebmb,image.tag=latest helm/devops
