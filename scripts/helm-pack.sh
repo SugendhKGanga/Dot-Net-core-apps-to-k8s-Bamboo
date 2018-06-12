340fa61f95b591eee3c2ffb526871299af5e299f
sugendhkganga <sugendhkganga@gmail.com>
2018-06-12 16:43:59 +0530

    helm scripts

---
+++ scripts/helm-pack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Helm pack
+    echo -e "\nPacking Helm chart"
+
+    [ -d pkg/helm ] && rm -rf pkg/helm
+    mkdir -p pkg/helm
+
+    helm package -d pkg/helm helm/devops || errorExit "Packing helm chart helm/devops failed"
+
