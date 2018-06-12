47554872e00d4c9cbcf9ecc5241613dd3a624e85
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2018-06-12 19:11:37 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@
 #sleep 60
 #kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n test
 
-rm -rf helm
-/usr/local/bin/helm repo add helm http://35.224.229.155/artifactory/helm-local
-/usr/local/bin/helm repo update
+#rm -rf helm
+#/usr/local/bin/helm repo add helm http://35.224.229.155/artifactory/helm-local
+#/usr/local/bin/helm repo update
 /usr/local/bin/helm upgrade --install --namespace test dotnetcorebmb-latest-test --set image.repository=sugendh/dotnetcorebmb,image.tag=latest helm/devops
