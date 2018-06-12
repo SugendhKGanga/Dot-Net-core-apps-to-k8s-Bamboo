16cc7ae7bb464c006a7bbd80c039eb1ca3095cfd
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2018-06-13 01:06:58 +0530

    Update update_helm_version.sh

--- scripts/update_helm_version.sh
+++ scripts/update_helm_version.sh
@@ -4,4 +4,4 @@ buildNumber=$bamboo_buildNumber
 
 version="0."+$buildNumber+".0"
 
-sed 's/versionhere/$version/g' ./helm/devops/Chart.yaml
+sed -i "s/versionhere/$version/g" ./helm/devops/Chart.yaml
