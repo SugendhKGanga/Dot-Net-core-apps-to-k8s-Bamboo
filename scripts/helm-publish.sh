3cfe3159be47879c8c21f0dcdc88720cece6c572
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2018-06-12 23:22:41 +0530

    Update helm-publish.sh

--- scripts/helm-publish.sh
+++ scripts/helm-publish.sh
@@ -3,8 +3,8 @@
 # Pushing the Helm chart
 # Note - this uses the Artifactory API. You can replace it with any other solution you use.
 
-#HELM_USR="admin"
-#HELM_PSW="AP5hcKwvDY7Amewp"
+HELM_USR=${bamboo.HELM_USR}
+HELM_PSW=${bamboo.HELM_PSW}
 #HELM_REPO="http://23.101.135.43/artifactory/helm-local/"
 
     echo -e "\nPushing Helm chart"
@@ -13,4 +13,4 @@
     echo "Helm chart: ${chart_name}"
 
     [ ! -z "${chart_name}" ] || errorExit "Did not find the helm chart to deploy"
-    curl -u${bamboo.HELM_USR}:${bamboo.HELM_PSW} -T ${chart_name} "${bamboo.HELM_REPO}/$(basename ${chart_name})" || errorExit "Uploading helm chart failed"
+    curl -u$HELM_USR:$HELM_PSW -T ${chart_name} "${bamboo.HELM_REPO}/$(basename ${chart_name})" || errorExit "Uploading helm chart failed"
